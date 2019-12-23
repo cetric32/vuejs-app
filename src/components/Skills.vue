474089d0dcad4cbc49b04108f3ff725b487ae8e4
cetric <cetric32@gmail.com>
2019-12-23 11:20:09 +0300

    my vuejs skills app
     Changes to be committed:
    	new file:   public/bootstrap/css/bootstrap-grid.css
    	new file:   public/bootstrap/css/bootstrap-grid.css.map
    	new file:   public/bootstrap/css/bootstrap-grid.min.css
    	new file:   public/bootstrap/css/bootstrap-grid.min.css.map
    	new file:   public/bootstrap/css/bootstrap-reboot.css
    	new file:   public/bootstrap/css/bootstrap-reboot.css.map
    	new file:   public/bootstrap/css/bootstrap-reboot.min.css
    	new file:   public/bootstrap/css/bootstrap-reboot.min.css.map
    	new file:   public/bootstrap/css/bootstrap.css
    	new file:   public/bootstrap/css/bootstrap.css.map
    	new file:   public/bootstrap/css/bootstrap.min.css
    	new file:   public/bootstrap/css/bootstrap.min.css.map
    	new file:   public/bootstrap/js/bootstrap.bundle.js
    	new file:   public/bootstrap/js/bootstrap.bundle.js.map
    	new file:   public/bootstrap/js/bootstrap.bundle.min.js
    	new file:   public/bootstrap/js/bootstrap.bundle.min.js.map
    	new file:   public/bootstrap/js/bootstrap.js
    	new file:   public/bootstrap/js/bootstrap.js.map
    	new file:   public/bootstrap/js/bootstrap.min.js
    	new file:   public/bootstrap/js/bootstrap.min.js.map
    	new file:   public/bootstrap/js/jquery/jquery-3.4.1.js
    	new file:   public/bootstrap/js/jquery/jquery-3.4.1.min.js
    	modified:   public/index.html
    	modified:   src/App.vue
    	deleted:    src/components/HelloWorld.vue
    	new file:   src/components/Skills.vue

---
+++ src/components/Skills.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="skills">
+    <div class="row">
+      <div class="col-md-6 offset-md-3 mt-5">
+        <form  class="from-group" @submit.prevent="addSkill">
+        <input class="form-control bg-dark text-white" type="text" v-model="skill" placeholder="Add Skill here...">
+        </form>
+      </div>     
+    </div>
+    <div class="row">   
+      <div class="col-md-6 offset-md-3 mt-3">
+              <ul  class="list-group">
+                <li class="list-group-item" v-for="(data,index) in skills" :key="index">{{data.skill}}</li>
+            </ul> 
+      </div>       
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data(){
+    return {
+      skill:'',
+      skills:[
+        {'skill':'vue.js'},
+        {'skill':'Frontend Dev'}        
+      ]
+    }
+  },
+  methods:{
+    addSkill(){
+      this.skills.push({'skill':this.skill});
+      this.skill = '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
